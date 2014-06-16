87f0b452cff1aa88f7f10ad56712b15aa7814f67
Christophe Latour <clatour@ibitux.com>
2014-06-16 14:53:54 +0200

    indentation

--- tests/data/web.php
+++ tests/data/web.php
@@ -14,47 +14,55 @@
  */
 
 $config = array(
-	'components' => array(
-		'riak' => array(
-			'class' => 'sweelix\yii2\nosql\riak\Connection',
-			'dsn' => 'riak:dsn=http://192.168.1.123:8098',
-		),
-			
-		'riakError' => array(
-				'class' => 'sweelix\yii2\nosql\Connection',
-			'dsn' => 'YOUSHALLNOTPASS',
-		),
-		'nosqlError' => array(
-			'class' => 'sweelix\yii2\nosql\riak\Connection',
-			'dsn' => 'nosql:dsn=http://192.168.1.123:8098'
-		),
+    'components' => array(
+        'riak' => array(
+            'class' => 'sweelix\yii2\nosql\riak\Connection',
+            'dsn' => 'riak:dsn=http://192.168.1.123:8098'
+        ),
 
-		'riakcs' => array(
-			'class' => 'sweelix\yii2\nosql\riakcs\Connection',
-			'proxy' => '192.168.1.123:8080',
-			'accessKey' => 'STEIORR72X2J0HBFQ-GD',
-			'secretKey' => 't_tfgGSvxYcJAmdjguGYqwJt8CuB8V8xNyfNqQ==',
-			'useSsl' => false,
-			'useExceptions' => false,
-		),
-		'riakcsError' => array(
-			'class' => 'sweelix\yii2\nosql\riakcs\Connection',
-			'proxy' => 'testForFail', //Will fail to connect
-			'accessKey' => 'STEIORR72X2J0HBFQ-GD',
-			'secretKey' => 't_tfgGSvxYcJAmdjguGYqwJt8CuB8V8xNyfNqQ==',
-			'useSsl' => false,
-			'useExceptions' => false,	
-		),
-		'log' => array(
-			'traceLevel' => YII_DEBUG ? 3 : 0,
-			'targets' => array(
-				array(
-					'class' => 'yii\log\FileTarget',
-					'levels' => array('error', 'warning', 'trace', 'info'),
-				),
-			),
-		),
-	),
+        'riakError' => array(
+            'class' => 'sweelix\yii2\nosql\Connection',
+            'dsn' => 'YOUSHALLNOTPASS'
+        ),
+        'nosqlError' => array(
+            'class' => 'sweelix\yii2\nosql\riak\Connection',
+            'dsn' => 'nosql:dsn=http://192.168.1.123:8098'
+        ),
+
+        'riakcs' => array(
+            'class' => 'sweelix\yii2\nosql\riakcs\Connection',
+            'proxy' => '192.168.1.123:8080',
+            'accessKey' => 'STEIORR72X2J0HBFQ-GD',
+            'secretKey' => 't_tfgGSvxYcJAmdjguGYqwJt8CuB8V8xNyfNqQ==',
+            'useSsl' => false,
+            'useExceptions' => false
+        ),
+        'riakcsError' => array(
+            'class' => 'sweelix\yii2\nosql\riakcs\Connection',
+            'proxy' => 'testForFail', // Will
+                                      // fail
+                                      // to
+                                      // connect
+            'accessKey' => 'STEIORR72X2J0HBFQ-GD',
+            'secretKey' => 't_tfgGSvxYcJAmdjguGYqwJt8CuB8V8xNyfNqQ==',
+            'useSsl' => false,
+            'useExceptions' => false
+        ),
+        'log' => array(
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => array(
+                array(
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => array(
+                        'error',
+                        'warning',
+                        'trace',
+                        'info'
+                    )
+                )
+            )
+        )
+    )
 );
 
 return $config;
